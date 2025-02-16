1d693e366735d60c9df371449b715c40cc801d2e
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-02-16 14:34:01 +0900

    Create Empty.cpp

---
+++ Empty.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string str1("");
+  cout << boolalpha; // 1,0을 true, false로 출력 기능
+  cout << str1.empty() << endl; //str1이 비어있냐?
+}
