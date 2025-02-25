3609896dfe6d852eaa313d7d220bc92f6f79c997
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-02-25 14:48:53 +0900

    Create bitAND.cpp

---
+++ bitAND.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main()
+{
+    int a = 13;
+    int b = 27;
+    int c = a & b; // 비트 and 연산
+
+    cout << bitset<8>(a) << endl;
+    cout << bitset<8>(b) << endl;
+    cout << bitset<8>(c) << endl;
+
+    return 0;
+}
+
+/* 출력값
+00001101
+00011011
+00001001
+*/
