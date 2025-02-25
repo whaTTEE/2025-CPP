ea0e50b6a0633929520a87690176f6de6b47210c
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-02-25 12:14:36 +0900

    Create bool.cpp

---
+++ bool.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    bool value;
+
+    value = true;
+    cout<< value << endl;
+
+    value = false;
+    cout << value << endl;
+
+    return 0;
+}
+
+bool 타입 true > 1 , false > 0
