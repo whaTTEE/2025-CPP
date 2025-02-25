2416fa4344c14071773dd437ed3f9a66f370e19b
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-02-25 16:26:49 +0900

    Create Function.cpp

---
+++ Function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void function(int x, int y){
+    int result = x + y;
+    cout << result << endl;
+}
+
+int main() {
+   function(3,4);
+    
+   return 0;
+}
