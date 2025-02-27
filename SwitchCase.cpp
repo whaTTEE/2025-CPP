d2c81548999b3e4ffd6b60d49de09cac3bd590cc
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-02-27 15:48:47 +0900

    Create SwitchCase.cpp

---
+++ SwitchCase.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int a;
+cin >> a;
+
+switch (a)
+    {
+        case 1:
+        cout << "1입니다" << endl;
+        break;
+        case 2:
+        cout << "2입니다" << endl;
+        break;
+        default:
+        cout << "다시 입력" << endl;
+        break;
+    }    
+}
