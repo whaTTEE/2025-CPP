4e557216da894e29f3d45ca4c5640f060f06a8f9
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-02-27 15:55:29 +0900

    Create While.cpp

---
+++ While.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int variable = 0;
+    while(variable < 5)
+    {
+        cout << variable << endl;
+        variable++;
+    }
+    return 0;
+}
