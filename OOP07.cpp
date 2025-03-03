5342f0bc293c473667aaf99fbcf3ff6f31fd68da
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-03-03 16:10:37 +0900

    Create OOP07.cpp

---
+++ OOP07.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+
+class car {
+private:
+
+
+public:
+string name;
+int option;
+string color;
+car(string a, int b){
+    name = a;
+    option = b;
+}
+
+car(string a){
+    color = a;
+}
+    virtual void function(){
+    cout << name << " " << option << endl;
+    }
+    void function2(){
+     cout << color << endl;
+    }
+};
+
+class autonomous_car : public car{
+public:
+autonomous_car(string a, int b) : car(a, b){}
+
+ void function() override{
+    cout << name << " autonomous " << option << endl;
+}
+};
+
+int main() {
+    car car1("Ionique5",5);
+    car car3("white");
+    car1.function();
+    car3.function2();
+    autonomous_car car2("Autonomous-Ionique5",10);
+    car2.function();
+    
+
+}
