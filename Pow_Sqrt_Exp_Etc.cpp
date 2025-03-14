73d84d9f49c45f233929cbbde052169bc91d72ae
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-03-14 14:56:27 +0900

    Create Pow_Sqrt_Exp_Etc.cpp

---
+++ Pow_Sqrt_Exp_Etc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <numbers>
+
+using namespace std;
+
+double x = 4;
+double y = 3;
+int z = 30;
+
+int main(){
+    
+cout << pow(x,y) << endl;
+cout << sqrt(x) << endl;
+cout << exp(x) << endl;
+cout << sin(z) << endl;
+cout << log(1) << endl;
+}
+
