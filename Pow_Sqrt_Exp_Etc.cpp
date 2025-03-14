31c8e7664b64f6a52c6a1a384b6e55efd4483afa
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-03-14 15:02:44 +0900

    Update Pow_Sqrt_Exp_Etc.cpp

--- Pow_Sqrt_Exp_Etc.cpp
+++ Pow_Sqrt_Exp_Etc.cpp
@@ -12,8 +12,12 @@ int main(){
     
 cout << pow(x,y) << endl;
 cout << sqrt(x) << endl;
-cout << exp(x) << endl;
+cout << ceil(exp(x)) << endl;
+cout << " " << endl;
 cout << sin(z) << endl;
+cout << floor(sin(z)) << endl;
+cout << ceil(sin(z)) << endl;
+cout << " " << endl;
 cout << log(1) << endl;
 }
 
